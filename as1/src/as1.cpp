93598ae25a231b3563dd17b70e2f9a30a3dea3b1
colbygram <cgram79@yahoo.com>
2024-02-08 20:51:19 -0800

    Update 1.3: Model rendered

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -5,7 +5,14 @@
 //Extra Credit: 
 
 #include <iostream>
+#include <unordered_map>
 #include "../raylib-cpp/include/raylib-cpp.hpp"
+
+enum MESH_FILES{
+  PLANE = 0,
+  BOAT,
+  TANKER
+};
 
 int main(void)
 {
@@ -15,29 +22,42 @@ int main(void)
   const std::string texture_file_path = "../assets/textures/";
   const std::string mesh_file_path = "../assets/meshes/";
 
+  std::unordered_map<MESH_FILES, std::string> file_map;
+  file_map[PLANE] = "PolyPlane.glb";
+  file_map[BOAT] = "SmitHouston_Tug.glb";
+  file_map[TANKER] = "OilTanker.glb";
+
   //Raylib Objects
   raylib::Window window(WIDTH, HEIGHT, "CS 381 - Assignment 1");
-  raylib::Camera main_camera(raylib::Vector3(-3,3,3), raylib::Vector3(0,0,0), raylib::Vector3::Up(), 90, CAMERA_PERSPECTIVE);
+  raylib::Camera main_camera(raylib::Vector3(-50,50,50), raylib::Vector3(0,0,0), raylib::Vector3::Up(), 90, CAMERA_PERSPECTIVE);
+  raylib::Model model;
+  raylib::BoundingBox box;
 
+  model.Load(mesh_file_path + file_map[PLANE]);
+  box = GetModelBoundingBox(model);
   while (!window.ShouldClose())
   {
-    int currentHeight = window.GetRenderHeight();
-    int currentWidth = window.GetRenderWidth();
+    UpdateCamera(&main_camera, CAMERA_ORBITAL);
 
     //Begin Rendering
     window.BeginDrawing();
     {
-        main_camera.BeginMode();
-        {
-            DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(3,0,0), RED);//X-axis
-            DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(0,3,0), YELLOW); //Y-axis
-            DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(0,0,3), BLUE); // Z-axis
-        }
-        main_camera.EndMode();
+      ClearBackground(GRAY);
+      main_camera.BeginMode();
+      {
+          DrawGrid(100, 5);
+          DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(10,0,0), RED);//X-axis
+          DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(0,10,0), YELLOW); //Y-axis
+          DrawLine3D(raylib::Vector3(0,0,0),raylib::Vector3(0,0,10), BLUE); // Z-axis
+          DrawModel(model, raylib::Vector3(0,3,0), 1, WHITE);
+          DrawBoundingBox(box, WHITE);
+      }
+      main_camera.EndMode();
     }
     window.EndDrawing();
   }
 
+  UnloadModel(model);
   CloseWindow();
 
   return 0;
